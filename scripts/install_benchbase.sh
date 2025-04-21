aed6bcd61432e65656790fcbdf38fb3409ef0f9a
Jamshed Karimnazarov <karimnazarovj@gmail.com>
2025-04-21 18:34:28 +0000

    first run with tpch 0.1 scale

--- scripts/install_benchbase.sh
+++ scripts/install_benchbase.sh
@@ -1,4 +1,4 @@
-# sudo apt install openjdk-17-jdk 
+sudo apt install openjdk-21-jdk 
 git clone https://github.com/cmu-db/benchbase.git ../benchbase
 cd ../benchbase
 git checkout b4b36683afdf79dd8bf70b95199b874c68218975
